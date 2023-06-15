a55ab5c03cf9dad4f67a112edae6822589a523d6
Adam Patterson <adam@adamrt.com>
2023-06-14 19:56:59 -0700

    Draw triangle

--- src/main.c
+++ src/main.c
@@ -2,12 +2,13 @@
 #include "sokol_gfx.h"
 #include "sokol_log.h"
 #include "sokol_glue.h"
-
 #define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #include "cimgui.h"
 #include "sokol_imgui.h"
 
 static struct {
+    sg_pipeline pipe;
+    sg_bindings bind;
     sg_pass_action pass_action;
 } state;
 
@@ -18,9 +19,56 @@ static void init(void) {
     });
     simgui_setup(&(simgui_desc_t){ 0 });
 
+    /* a vertex buffer */
+    const float vertices[] = {
+        // positions            // colors
+         0.0f,  0.5f, 0.5f,     1.0f, 0.0f, 0.0f, 1.0f,
+         0.5f, -0.5f, 0.5f,     0.0f, 1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f,     0.0f, 0.0f, 1.0f, 1.0f
+    };
+
+    state.bind.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
+        .data = SG_RANGE(vertices),
+        .label = "triangle-vertices"
+        });
+
+    // create shader from code-generated sg_shader_desc
+    sg_shader shd = sg_make_shader(&(sg_shader_desc){
+            .vs.source =
+                "#version 330\n"
+                "layout(location=0) in vec4 position;\n"
+                "layout(location=1) in vec4 color0;\n"
+                "out vec4 color;\n"
+                "void main() {\n"
+                "  gl_Position = position;\n"
+                "  color = color0;\n"
+                "}\n",
+            .fs.source =
+                "#version 330\n"
+                "in vec4 color;\n"
+                "out vec4 frag_color;\n"
+                "void main() {\n"
+                "  frag_color = color;\n"
+                "}\n"
+        });
+
+    // create a pipeline object (default render states are fine for triangle)
+    state.pipe = sg_make_pipeline(&(sg_pipeline_desc){
+        .shader = shd,
+        // if the vertex layout doesn't have gaps, don't need to provide strides
+        // and offsets
+        .layout = {
+            .attrs = {
+                [0].format = SG_VERTEXFORMAT_FLOAT3,
+                [1].format = SG_VERTEXFORMAT_FLOAT4
+            }
+        },
+        .label = "triangle-pipeline"
+        });
+
     // initial clear color
     state.pass_action = (sg_pass_action) {
-        .colors[0] = { .load_action = SG_LOADACTION_CLEAR, .clear_value = { 0.0f, 0.5f, 1.0f, 1.0 } }
+        .colors[0] = { .load_action = SG_LOADACTION_CLEAR, .clear_value = { 0.0f, 0.0f, 0.0f, 1.0 } }
     };
 }
 
@@ -43,6 +91,9 @@ static void frame(void) {
     gui();
 
     sg_begin_default_pass(&state.pass_action, sapp_width(), sapp_height());
+    sg_apply_pipeline(state.pipe);
+    sg_apply_bindings(&state.bind);
+    sg_draw(0, 3, 1);
 
     simgui_render();
     sg_end_pass();
