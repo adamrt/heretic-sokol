68c5b01631ef1b61c185c42bbe4cbbc509d8f072
Adam Patterson <adam@adamrt.com>
2023-06-30 18:23:29 -0700

    Learn OpenGL course 'Camera' with FPS Camera
    https://learnopengl.com/Getting-started/Camera

--- src/main.c
+++ src/main.c
@@ -43,9 +43,7 @@ static struct {
 
     struct {
         HMM_Vec3 position;
-        HMM_Vec3 target;
-        HMM_Vec3 direction;
-        HMM_Vec3 right;
+        HMM_Vec3 front;
         HMM_Vec3 up;
     } camera ;
 
@@ -86,14 +84,9 @@ static void init(void) {
     simgui_setup(&(simgui_desc_t){ 0 });
 
     state.vs_params.uSlider = 0.2f;
-    /* state.camera.position = HMM_V3(0.0f, 0.0f, 3.0f); */
-    /* state.camera.target = HMM_V3(0.0f, 0.0f, 0.0f); */
-    /* // The name direction vector is not the best chosen name, since it is actually pointing in the reverse direction of what it is targeting. */
-    /* state.camera.direction = HMM_NormV3(HMM_SubV3(state.camera.position, state.camera.target)); */
-
-    /* HMM_Vec3 up = HMM_V3(0.0f, 1.0f, 0.0f); */
-    /* state.camera.right = HMM_NormV3(HMM_Cross(up, state.camera.target)); */
-    /* state.camera.up = HMM_Cross(state.camera.direction, state.camera.right); */
+    state.camera.position = HMM_V3(0.0f, 0.0f, 3.0f);
+    state.camera.front = HMM_V3(0.0f, 0.0f, -1.0f);
+    state.camera.up = HMM_V3(0.0f, 0.1f, 0.0f);
 
     // vertex buffer object
     vertex vertices[] = {
@@ -230,6 +223,20 @@ static void event(const sapp_event* ev) {
         if (ev->key_code == SAPP_KEYCODE_ESCAPE) {
             sapp_quit();
         }
+
+        const float cameraSpeed = 0.05f; // adjust accordingly
+        if (ev->key_code == SAPP_KEYCODE_W) {
+            state.camera.position = HMM_AddV3(state.camera.position, HMM_MulV3F(state.camera.front, cameraSpeed));
+        }
+        if (ev->key_code == SAPP_KEYCODE_S) {
+            state.camera.position = HMM_SubV3(state.camera.position, HMM_MulV3F(state.camera.front, cameraSpeed));
+        }
+        if (ev->key_code == SAPP_KEYCODE_A) {
+            state.camera.position = HMM_SubV3(state.camera.position, HMM_MulV3F(HMM_NormV3(HMM_Cross(state.camera.front, state.camera.up)), cameraSpeed));
+        }
+        if (ev->key_code == SAPP_KEYCODE_D) {
+            state.camera.position = HMM_AddV3(state.camera.position, HMM_MulV3F(HMM_NormV3(HMM_Cross(state.camera.front, state.camera.up)), cameraSpeed));
+        }
     }
 }
 
@@ -256,11 +263,7 @@ static void frame(void) {
         HMM_V3(-1.3f,  1.0f, -1.5f)
     };
 
-
-    const float radius = 10.0f;
-    float camX = sin(state.delta) * radius;
-    float camZ = cos(state.delta) * radius;
-    state.vs_params.view = HMM_LookAt_RH(HMM_V3(camX, 0.0f, camZ), HMM_V3(0.0f, 0.0f, 0.0f), HMM_V3(0.0f, 0.1f, 0.0f));
+    state.vs_params.view = HMM_LookAt_RH(state.camera.position, HMM_AddV3(state.camera.position, state.camera.front), state.camera.up);
     state.vs_params.projection = HMM_Perspective_RH_NO(HMM_AngleDeg(45.0f), sapp_widthf() / sapp_heightf(), 0.1f, 100.0f);
 
     igSetNextWindowPos((ImVec2){10,10}, ImGuiCond_Once, (ImVec2){0,0});
